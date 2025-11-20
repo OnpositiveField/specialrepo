8fcd9fae0761b9e942090972b10f8129c92553e0
olka2024 <156855542+olka2024@users.noreply.github.com>
2025-11-20 10:46:27 +0300

    Update second_module.py

--- second_module.py
+++ second_module.py
@@ -3,13 +3,13 @@ from python_math import circle_area, factorial, quadratic_equation
 def use_math_functions():
     print("Вызов функции circle_area() без пользовательского ввода:")
     # Можно вызвать с input, или изменить в python_math.py, чтобы принимать параметры вместо интерактива
-    # circle_area()  # вызывает input()
+    circle_area()  # вызывает input()
     
     print("Вызов функции factorial() без пользовательского ввода:")
-    # factorial()  # вызывает input()
+    factorial()  # вызывает input()
     
     print("Вызов функции quadratic_equation() без пользовательского ввода:")
-    # quadratic_equation()  # вызывает input()
+    quadratic_equation()  # вызывает input()
     
     print("Вы можете модифицировать python_math.py, чтобы функции принимали параметры.")
 
