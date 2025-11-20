51fc1e83f8a2d229a8e4c528f311b43e97a5b647
olka2024 <156855542+olka2024@users.noreply.github.com>
2025-11-20 10:49:27 +0300

    Update third_module.py

--- third_module.py
+++ third_module.py
@@ -1,1 +1,9 @@
 
+def factorial(n):
+    try:
+        if n < 0:
+            print("Ошибка: факториал определён только для неотрицательных чисел!")
+        else:
+            print(f"Факториал {n}: {math.factorial(n)}")
+    except ValueError:
+        print("Ошибка: введите целое число!")
